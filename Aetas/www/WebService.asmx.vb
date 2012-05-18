edf929f20b4346b3265a381624d0260ee3a31448
Eystein Bye <eysteinbye@gmail.com>
2012-05-18 10:27:02 +0200

    Removing all that I dont need

--- Aetas/www/WebService.asmx.vb
+++ Aetas/www/WebService.asmx.vb
@@ -13,7 +13,7 @@ Public Class WebService
 	Inherits System.Web.Services.WebService
 
 	Private _TestJSON As String = "{  ""timeline"": {""asset"": {""caption"": ""Caption text goes here"", ""credit"": ""Credit Name Goes Here"", ""media"": ""http://yourdomain_or_socialmedialink_goes_here.jpg""}, ""date"": [{""asset"": {""caption"": ""Caption text goes here"", ""credit"": ""Credit Name Goes Here"", ""media"": ""http://twitter.com/ArjunaSoriano/status/164181156147900416""}, ""endDate"": ""2011,12,11"", ""headline"": ""Headline Goes Here"", ""startDate"": ""2011,12,10"", ""text"": ""<p>Body text goes here, some HTML is OK</p>""}], ""headline"": ""The Main Timeline Headline Goes here"", ""startDate"": ""1888"", ""text"": ""<p>Intro body text goes here, some HTML is ok</p>"", ""type"": ""default""}}"
-	Private _TestObject As New TestClass
+	'Private _TestObject As New TestClass
 
 	<WebMethod()> _
 	  <ScriptMethod(ResponseFormat:=ResponseFormat.Json, UseHttpGet:=True)>
@@ -27,26 +27,26 @@ Public Class WebService
 		Return _TestJSON
 	End Function
 
-	<WebMethod()> _
-	<ScriptMethod(ResponseFormat:=ResponseFormat.Json, UseHttpGet:=True)>
-	Public Function getDynaminJSON() As String
+	'<WebMethod()> _
+	'<ScriptMethod(ResponseFormat:=ResponseFormat.Json, UseHttpGet:=True)>
+	'Public Function getDynaminJSON() As String
 
-		Dim aa As New System.Web.Script.Serialization.JavaScriptSerializer
-		Dim ee As StringBuilder = Nothing
-		aa.Serialize(_TestObject, ee)
+	'	Dim aa As New System.Web.Script.Serialization.JavaScriptSerializer
+	'	Dim ee As StringBuilder = Nothing
+	'	aa.Serialize(_TestObject, ee)
 
-		Return ee.ToString
-	End Function
+	'	Return ee.ToString
+	'End Function
 
 End Class
 
 
-Public Class TestClass
-	Public Property Id As Integer
-	Public Property Name As String
+'Public Class TestClass
+'	Public Property Id As Integer
+'	Public Property Name As String
 
-	Public Sub New()
-		Id = 23
-		Name = "Demo"
-	End Sub
-End Class
+'	Public Sub New()
+'		Id = 23
+'		Name = "Demo"
+'	End Sub
+'End Class
