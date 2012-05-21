197f3fc4c80a546d8720bed8f5b756e39529939c
Eystein Bye <eysteinbye@gmail.com>
2012-05-21 14:23:18 +0200

    Added new Raven webservice

--- Aetas/www/WebService.asmx.vb
+++ Aetas/www/WebService.asmx.vb
@@ -4,8 +4,7 @@ Imports System.ComponentModel
 Imports System.Web.Script.Services
 Imports System.Data.SqlClient
 Imports System.Data
-Imports Raven.Client.Document
-Imports Raven.Client
+
 
 
 <System.Web.Script.Services.ScriptService()> _
@@ -183,55 +182,7 @@ Public Class WebService
 
 
 
-	<WebMethod()> _
-	<ScriptMethod(ResponseFormat:=ResponseFormat.Json, UseHttpGet:=True)>
-	Public Function RavendbSave() As String
-
-
-
-
-		Dim store As New DocumentStore() With {.ConnectionStringName = "https://1.ravenhq.com/databases/AppHarbor_0c9d6757-e342-4494-abde-ea634062980f"}
-		store.Initialize()
-
-
-
-
-		Dim assets As New Assets
-		assets.media = "http://upload.wikimedia.org/wikipedia/commons/9/98/Pablo_picasso_1.jpg"
-		assets.credit = "Eystein Bye"
-		assets.caption = "from Wikipedia"
-
-		Dim events As New Events
-		events.headline = "Pablo Picasso"
-		events.text = "<p>a Spanish painter, sculptor, printmaker, ceramicist, and stage designer who spent most of his adult life in France</p>"
-		events.asset = assets
-		events.startDate = "1881,10,25"
-		events.endDate = "1973,04,08"
-
-
-		Using session As IDocumentSession = store.OpenSession()
-			session.Store(events)
-			session.SaveChanges()
-		End Using
-
-		Return ""
-
-	End Function
-
 
 
 End Class
 
-Public Class Events
-	Public Property headline() As String
-	Public Property text() As String
-	Public Property asset() As Assets
-	Public Property startDate() As String
-	Public Property endDate() As String
-End Class
-
-Public Class Assets
-	Public Property media() As String
-	Public Property credit() As String
-	Public Property caption() As String
-End Class
